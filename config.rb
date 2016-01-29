fa99e79b37bcb1d4420eb373a6212ee915770f07
Joey Cody <joeydcody@gmail.com>
2016-01-28 18:28:05 -0800

    add back in google analytics tracking

--- config.rb
+++ config.rb
@@ -1,77 +1,48 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-activate :livereload
-activate :directory_indexes
-
-configure :build do
-  activate :favicon_maker
-end
-
 # activate :blog do |blog|
+#   # This will add a prefix to all links, template references and source paths
 #   blog.prefix = "blog"
-# end
 
+#   # blog.permalink = "{year}/{month}/{day}/{title}.html"
+#   # Matcher for blog source files
+#   # blog.sources = "{year}-{month}-{day}-{title}.html"
+#   # blog.taglink = "tags/{tag}.html"
+#   # blog.layout = "layout"
+#   # blog.summary_separator = /(READMORE)/
+#   # blog.summary_length = 250
+#   # blog.year_link = "{year}.html"
+#   # blog.month_link = "{year}/{month}.html"
+#   # blog.day_link = "{year}/{month}/{day}.html"
+#   # blog.default_extension = ".markdown"
 
-###
-# Page options, layouts, aliases and proxies
-###
+#   blog.tag_template = "tag.html"
+#   blog.calendar_template = "calendar.html"
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
+#   # Enable pagination
+#   # blog.paginate = true
+#   # blog.per_page = 10
+#   # blog.page_link = "page/{num}"
 # end
 
 require "helpers"
 
+# Pretty URLs as Directory Indexes, instead of file extensions
+activate :directory_indexes
+
 set :build_dir, "tmp"
-set :css_dir, "assets/css"
-set :js_dir, "assets/js"
-set :images_dir, "assets/img"
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
 
 # Build-specific configuration
 configure :build do
+  activate :favicon_maker
   activate :minify_css
   activate :minify_javascript
 
-
   activate :relative_assets
+end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+# Development-specific configuration
+configure :development do
+  activate :livereload
 end
